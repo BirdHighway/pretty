2272c573714396df5ebd979abe7231f740d6276f
Stephen Hyde <shh2112@gmail.com>
2021-12-12 19:31:03 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let data = '';
+
+process.stdin.on('data', (chunk) => {
+  data += chunk;
+});
+
+process.stdin.on('end', () => {
+  processData(data);
+});
+
+const processData = (data) => {
+  const json = JSON.parse(data);
+  const output = JSON.stringify(json, null, 2);
+  console.log(output);
+};
